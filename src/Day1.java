df1209d2fc09804241f7b23dfbb76e222591900b
Sophia Pham <sophia_pham@hotmail.com>
2020-12-01 12:29:07 +0100

    comments

--- src/Day1.java
+++ src/Day1.java
@@ -2,23 +2,25 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class Day1 {
     public static void main(String[] args) throws FileNotFoundException {
+        //get the input from file
         File file = new File("C:\\Users\\Pixie Waffle\\Desktop\\adventofcode2020\\adventofcode2020\\textfiles\\day1-input.txt");
         Scanner sc = new Scanner(file);
 
         int sum = 2020;
         List<Integer> integers = new ArrayList<>();
 
+        //add all the integers into a list
         while (sc.hasNextInt()) {
                 integers.add(sc.nextInt());
         }
 
+        //check if the list contains two elements that adds up to the sum
         for (int j = 0; j < integers.size(); j++){
             if (integers.contains(sum-integers.get(j))){
                 int expenseReport = integers.get(j) * (sum-integers.get(j));
