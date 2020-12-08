e58417fbab01070374adcd19adf689d2fa744090
Sophia Pham <sophia_pham@hotmail.com>
2020-12-08 13:16:51 +0100

    Day4 Code Cleanup

--- src/Day4.java
+++ src/Day4.java
@@ -33,73 +33,55 @@ public class Day4 {
         for(int i = 0; i < passportsWithKeyValue.size(); i++){
             int correctFields = 0;
             for (int j = 0; j < passportsWithKeyValue.get(i).size(); j++){
+                String key = passportsWithKeyValue.get(i).get(j).getKey();
+                String val = passportsWithKeyValue.get(i).get(j).getValue();
                 //Check is not Null or Empty Value
-                if (isNotNullOrEmpty(passportsWithKeyValue.get(i).get(j).getValue())){
-                    //Check Birth Year
-                    if (passportsWithKeyValue.get(i).get(j).getKey().equals("byr")){
-                        int byrValue = Integer.parseInt(passportsWithKeyValue.get(i).get(j).getValue());
-                        if (byrValue >= 1920 && byrValue <= 2002){
-                            correctFields++;
-                        }
-                    }
-                    //Check Issue Year
-                    if (passportsWithKeyValue.get(i).get(j).getKey().equals("iyr")){
-                        int iyrValue = Integer.parseInt(passportsWithKeyValue.get(i).get(j).getValue());
-                        if (iyrValue >= 2010 && iyrValue <= 2020){
-                            correctFields++;
-                        }
-                    }
-                    //Check Expiration Year
-                    if (passportsWithKeyValue.get(i).get(j).getKey().equals("eyr")){
-                        int eyrValue = Integer.parseInt(passportsWithKeyValue.get(i).get(j).getValue());
-                        if (eyrValue >= 2020 && eyrValue <= 2030){
-                            correctFields++;
-                        }
-                    }
-                    //Check Height
-                    if(passportsWithKeyValue.get(i).get(j).getKey().equals("hgt")){
-                        String hgtValue = passportsWithKeyValue.get(i).get(j).getValue();
-                        String cmOrIn = hgtValue.substring(hgtValue.length()-2);
-                        int height = 0;
-                        if(hgtValue.length() > 2)
-                            height = Integer.parseInt(hgtValue.substring(0, hgtValue.length()-2));
-                        if (cmOrIn.equals("cm")){
-                            if(height >= 150 && height <= 193) correctFields++;
-                        }
-                        if (cmOrIn.equals("in")){
-                            if(height >= 59 && height <= 76) correctFields++;
-                        }
+                if (isNotNullOrEmpty(val)){
+                    //Check Birth Year/Issue Year/Expiration Year
+                    if(isCorrectYear(key, val))
+                        correctFields++;
+
                     }
 
-                    //Check Hair Color
-                    if(passportsWithKeyValue.get(i).get(j).getKey().equals("hcl")){
-                        String hclValue = passportsWithKeyValue.get(i).get(j).getValue();
-                        String hashTag = hclValue.substring(0, 1);
-                        String hairColor = hclValue.substring(1);
-                        if (hashTag.equals("#") && hairColor.length() == 6) {
-                            correctFields++;
-                        }
+                //Check Height
+                if(key.equals("hgt")){
+                    String cmOrIn = val.substring(val.length()-2);
+                    int height = 0;
+                    if(val.length() > 2)
+                        height = Integer.parseInt(val.substring(0, val.length()-2));
+                    if (cmOrIn.equals("cm")){
+                        if(height >= 150 && height <= 193) correctFields++;
                     }
-
-                    //Check Eye Color
-                    if(passportsWithKeyValue.get(i).get(j).getKey().equals("ecl")){
-                        String eclValue = passportsWithKeyValue.get(i).get(j).getValue();
-                        if (eclValue.equals("amb") || eclValue.equals("blu") || eclValue.equals("brn") ||
-                                eclValue.equals("gry") || eclValue.equals("grn") || eclValue.equals("hzl") ||
-                                eclValue.equals("oth"))
-                        {
-                            correctFields++;
-                        }
+                    if (cmOrIn.equals("in")){
+                        if(height >= 59 && height <= 76) correctFields++;
                     }
+                }
 
-                    //Check Passport ID
-                    if(passportsWithKeyValue.get(i).get(j).getKey().equals("pid")){
-                        String pidValue = passportsWithKeyValue.get(i).get(j).getValue();
-                        if(pidValue.length() == 9) {
-                            int pid = Integer.parseInt(pidValue);
-                            if (pid > 0){
-                                correctFields++;
-                            }
+                //Check Hair Color
+                if(key.equals("hcl")){
+                    String hashTag = val.substring(0, 1);
+                    String hairColor = val.substring(1);
+                    if (hashTag.equals("#") && hairColor.length() == 6) {
+                        correctFields++;
+                    }
+                }
+
+                //Check Eye Color
+                if(key.equals("ecl")){
+                    if (val.equals("amb") || val.equals("blu") || val.equals("brn") ||
+                            val.equals("gry") || val.equals("grn") || val.equals("hzl") ||
+                            val.equals("oth"))
+                    {
+                        correctFields++;
+                    }
+                }
+
+                //Check Passport ID
+                if(key.equals("pid")){
+                    if(val.length() == 9) {
+                        int pid = Integer.parseInt(val);
+                        if (pid > 0){
+                            correctFields++;
                         }
                     }
                 }
@@ -109,6 +91,25 @@ public class Day4 {
             }
         }
         System.out.println(validPassports);
+    }
+
+    private static boolean isCorrectYear(String key, String value){
+        //Check Birth Year
+        if (key.equals("byr")){
+            int byrValue = Integer.parseInt(value);
+            return byrValue >= 1920 && byrValue <= 2002;
+        }
+        //Check Issue Year
+        if (key.equals("iyr")){
+            int iyrValue = Integer.parseInt(value);
+            return iyrValue >= 2010 && iyrValue <= 2020;
+        }
+        //Check Expiration Year
+        if (key.equals("eyr")){
+            int eyrValue = Integer.parseInt(value);
+            return eyrValue >= 2020 && eyrValue <= 2030;
+        }
+        return false;
     }
 
     private static boolean isNotNullOrEmpty(String str){
