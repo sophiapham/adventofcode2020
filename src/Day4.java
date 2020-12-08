287759d9cfe88f27f330b90270a3063d6a59fbfe
Sophia Pham <sophia_pham@hotmail.com>
2020-12-08 13:51:46 +0100

    swapped for switch cases instead

--- src/Day4.java
+++ src/Day4.java
@@ -47,56 +47,51 @@ public class Day4 {
     }
 
     private static boolean isCorrectField(String key, String val){
-        //Check Height
-        if(key.equals("hgt")){
-            String cmOrIn = val.substring(val.length()-2);
-            int height = 0;
-            if(val.length() > 2)
-                height = Integer.parseInt(val.substring(0, val.length()-2));
-            if (cmOrIn.equals("cm")){
-                return height >= 150 && height <= 193;
-            }
-            if (cmOrIn.equals("in")){
-                return height >= 59 && height <= 76;
-            }
+        switch (key) {
+            //Check Height
+            case "hgt":
+                String cmOrIn = val.substring(val.length()-2);
+                int height = 0;
+                if(val.length() > 2)
+                    height = Integer.parseInt(val.substring(0, val.length()-2));
+                if (cmOrIn.equals("cm")){
+                    return height >= 150 && height <= 193;
+                }
+                if (cmOrIn.equals("in")){
+                    return height >= 59 && height <= 76;
+                }
+            //Check Hair Color
+            case "hcl":
+                String hashTag = val.substring(0, 1);
+                String hairColor = val.substring(1);
+                return hashTag.equals("#") && hairColor.length() == 6;
+            //Check Eye Color
+            case "ecl":
+                return val.equals("amb") || val.equals("blu") || val.equals("brn") ||
+                        val.equals("gry") || val.equals("grn") || val.equals("hzl") ||
+                        val.equals("oth");
+            //Check Passport ID
+            case "pid":
+                return val.length() == 9 && Integer.parseInt(val) > 0;
+                default:
+                    //Check Birth Year/Issue Year/Expiration Year
+                    return isCorrectYear(key, val);
         }
-
-        //Check Hair Color
-        if(key.equals("hcl")){
-            String hashTag = val.substring(0, 1);
-            String hairColor = val.substring(1);
-            return hashTag.equals("#") && hairColor.length() == 6;
-        }
-
-        //Check Eye Color
-        if(key.equals("ecl")){
-            return val.equals("amb") || val.equals("blu") || val.equals("brn") ||
-                    val.equals("gry") || val.equals("grn") || val.equals("hzl") ||
-                    val.equals("oth");
-        }
-
-        //Check Passport ID
-        if(key.equals("pid")){
-            return val.length() == 9 && Integer.parseInt(val) > 0;
-        }
-        //Check Birth Year/Issue Year/Expiration Year
-        return isCorrectYear(key, val);
     }
     private static boolean isCorrectYear(String key, String value){
-        //Check Birth Year
-        if (key.equals("byr")){
-            int byrValue = Integer.parseInt(value);
-            return byrValue >= 1920 && byrValue <= 2002;
-        }
-        //Check Issue Year
-        if (key.equals("iyr")){
-            int iyrValue = Integer.parseInt(value);
-            return iyrValue >= 2010 && iyrValue <= 2020;
-        }
-        //Check Expiration Year
-        if (key.equals("eyr")){
-            int eyrValue = Integer.parseInt(value);
-            return eyrValue >= 2020 && eyrValue <= 2030;
+        switch (key){
+            //Check Birth Year
+            case "byr":
+                int byrValue = Integer.parseInt(value);
+                return byrValue >= 1920 && byrValue <= 2002;
+            //Check Issue Year
+            case "iyr":
+                int iyrValue = Integer.parseInt(value);
+                return iyrValue >= 2010 && iyrValue <= 2020;
+            //Check Expiration Year
+            case "eyr":
+                int eyrValue = Integer.parseInt(value);
+                return eyrValue >= 2020 && eyrValue <= 2030;
         }
         return false;
     }
